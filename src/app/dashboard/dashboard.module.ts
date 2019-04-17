f1302c67898468c541bdf786a64f1fbc0a18b277
husnul94 <hasanaharis94@gmail.com>
2019-04-17 11:25:29 +0800

    import shared module

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from "@angular/core";
 import {ExamplePage} from "./example/example.page";
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   bootstrap:[],
   providers: [],
   declarations: [ExamplePage],
-  imports:[],
+  imports:[SharedModule],
   exports: [],
   entryComponents: [],
 })
